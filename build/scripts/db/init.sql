03a8f9d8bcb39bff5048f1c548d6eb2e4216daea
Akshit Dhar <akshit@multipl.xyz>
2021-05-29 17:43:02 +0530

    added initial db script

---
+++ build/scripts/db/init.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS "vehicle_details" (
+    "id" serial,
+    "arrived_at" timestamptz,
+    "departed_at" timestamptz,
+    "reg_no" text ,             -- vehicle registration no
+    "type" int,
+    PRIMARY KEY ("id")
+);
+
+COMMIT;
