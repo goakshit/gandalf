bd5b0b8374d9d2a50ff361a6cf0387cfb27b9941
Akshit Dhar <akshit@multipl.xyz>
2021-05-30 13:23:38 +0530

    bug fix

--- internal/types/billing.go
+++ internal/types/billing.go
@@ -4,7 +4,7 @@ import "time"
 
 // VehicleDetails -
 type VehicleDetails struct {
-	ID         uint      `json:"id"`
+	ID         uint      `json:"id" gorm:"primaryKey"`
 	ArrivedAt  time.Time `json:"arrived_at"`
 	DepartedAt time.Time `json:"departed_at"`
 	RegNo      string    `json:"reg_no"`
